131042e5e989e932bf5ae553ec5bbebf91cf05a3
Christian Perfect <christianperfect@gmail.com>
2011-08-01 22:48:45 +0100

    cp's functional obtuse-o-version!
    Differences from Seb's version:
    
    - game state is not just stored in CSS - much simpler just to use an array inside a closure.
    - board is created in javascript when the game is started, because...
    - arbitrary board sizes are allowed
    - an unreasonably functional method of checking for reversi moves is used
    - copes with tied games
    - two players, so represent them with booleans
    - should allow several games to be created at once, and run independently
    
    - happy unicode snowmen!

---
+++ README.md
@@ -0,0 +1,14 @@
+Tic-Tac Othello
+===============
+
+A simple game based on Noughts and Crosses with additions from Othello.
+
+Based on @sebble's version.
+
+Instructions
+------------
+
+Click on a square to mark it with your symbol.
+The first player to mark an entire row, column, or proper diagonal with their symbol wins.
+
+Reversi rules apply - if you put your symbol on both ends of a line of the other guy's symbol, the whole line gets switched to your symbol.
--- tto.js
+++ tto.js
@@ -1,139 +1,197 @@
 $(function() {
-  $('#tto-game').data('player',0).data('playing',true);
 
-  $('.cell').each(function(i) {
-    $(this).data('player',999).data('col',(i)%4).data('row',(i-((i)%4))/4);
-  });
+//create an array of direction vectors to check for reversi moves
+//in a wilfully obtuse way
+var directions = [-1,0,1].reduce(function(l,a,i,arr){return l.concat(arr.reduce(function(l2,b){return l2.concat(a,b);},[]));},[]);
+directions.splice(8,2);
 
-  $('.cell').click(function(e) {
+// why not add a jQuery method to turn any selector into a tic-tac-reversi game?
+// WHY NOT INDEED
+$.fn.ttrGame = function(tsize) {
 
-    if ($('#tto-game').data('playing')==false) return;
+	$(this).each(function() {
+		var size = tsize;	//size is in scope of just this game, so won't get touched by other games in the same selector
 
-    var player = $('#tto-game').data('player');
-    var next = (player+1) % 2;
+		//clear the game area, add game styling
+		var elem = $(this);
+		elem.html('').addClass('ttrGame');
+		elem.undelegate('#board td:not(.player0,.player1)','click');
 
-    var row = $(this).data('row');
-    var col = $(this).data('col');
+		//create the buttons to make bigger and smaller 
+		elem.append('<input type="button" id="smaller" value="Smaller"/><input type="button" id="bigger" value="Bigger"/>');
+		elem.find('#smaller').click(function() {
+			size = Math.max(1,size-1)
+			elem.ttrGame(size);
+		});
+		elem.find('#bigger').click(function() {
+			size++;
+			elem.ttrGame(size);
+		});
 
-    if ($(this).data('player') == 999) {
-      $(this).addClass('player'+player);
-      $(this).data('player',player);
-      $('#tto-game').data('player',next);
-      
-      // now check othello
-      othello(row, col, player);
-      win(player);
-      nextp(player);
-    }
-  });
-});
+		//create the status display area
+		var player = $('<div id="status"/>').append('Player: ').append('<span id="name"/>');
+		elem.append(player);
 
-function othello(row, col, player) {
-  
-  var next = (player+1) % 2;
-  //console.log('Player: '+player+','+next);
-  //console.log('Position: '+row+','+col);
-  
-  function ttr_fn(x) { x = mod(x,8); return Math.floor((x+2)/3)-2*Math.floor(x/4)-Math.floor(x/5)-Math.floor(x/7); }
+		//create the board - both HTML and javascript array versions
+		var boardElem = $('<table id="board"/>');
+		var row;
+		var board = [];
+		for(var i=0;i<size;i++)
+		{
+			board.push([]);
+			row = $('<tr/>');
+			boardElem.append(row);
+			for(var j=0;j<size;j++)
+			{
+				row.append('<td/>');
+				board[i].push(null);
+			}
+		}
+		elem.append(boardElem);
 
-  function mod(x,y) { x = x%y; if (x<0) x=x+y; return x;}
+		//how many squares have been filled, and whether the game is over
+		var clicks = 0;
+		var done = false;
 
-  for (x=0;x<8;x++) {
-    var offr = ttr_fn(x);
-    var offc = ttr_fn(x-2);
-    var chkr = row + offr;
-    var chkc = col + offc;
-    var chkr2 = row + 2*offr;
-    var chkc2 = col + 2*offc;
-    var chkr3 = row + 3*offr;
-    var chkc3 = col + 3*offc;
-    //console.log('check: '+chkr+','+chkc);
-    //console.log($(':data("row='+chkr+'"):data("col='+chkc+'")').data('player'));
-    var n1 = $('#c'+chkr+chkc).data('player');
-    var n2 = $('#c'+chkr2+chkc2).data('player');
-    var n3 = $('#c'+chkr3+chkc3).data('player');
-    //var n1 = $(':data("row='+chkr+'"):data("col='+chkc+'")').data('player');
-    //var n2 = $(':data("row='+chkr2+'"):data("col='+chkc2+'")').data('player');
-    //var n3 = $(':data("row='+chkr3+'"):data("col='+chkc3+'")').data('player');
-    //console.log('Pos: '+chkr+','+chkc+' - '+n1);
-    //console.log('Pos: '+chkr2+','+chkc2+' - '+n2);
-    if (n1==next && n2==player) {
-      $('#c'+chkr+chkc).data('player',player).removeClass('player'+next).addClass('player'+player);
-      //console.log('Change');
-    }
-    if (n1==next && n2==next && n3==player) {
-      $('#c'+chkr+chkc).data('player',player).removeClass('player'+next).addClass('player'+player);
-      $('#c'+chkr2+chkc2).data('player',player).removeClass('player'+next).addClass('player'+player);
-      //console.log('Change');
-    }
-  }
+		//decide on a player to start, and set up
+		var player = Math.random()>0.5;
+		elem.find('#name').addClass(player ? 'player0' : 'player1');
+
+		//check for reversi moves - placing your mark on the end of a line of the other guy's, with one of yours at the other end
+		//goes horizontally, vertically, or diagonally
+		function checkReversi(i,j)
+		{
+			//for each direction in the array created at the top of the program
+			for(var m=0;m<16;m+=2)
+			{
+				var dx = directions[m];
+				var dy = directions[m+1];
+				var x = i+dx;
+				var y = j+dy;
+				var moves = 1;
+				//move in the given direction for as long as the cell you're looking at is owned by the other guy
+				while(x>=0 && x<size && y>=0 && y<size && board[x][y]==!player)
+				{
+					x += dx;
+					y += dy;
+					moves++;
+				}
+				//if process finished inside the board and on one of your squares, then a reversi move can be made
+				if(x>=0 && x<size && y>=0 && y<size && board[x][y]==player)
+				{
+					//for each cell in the line, make it yours
+					for(var n=1;n<moves;n++)
+					{
+						x = i+dx*n;
+						y = j+dy*n;
+						board[x][y] = player;
+						elem.find('#board tr').eq(x).find('td').eq(y).toggleClass('player0 player1');
+					}
+				}
+
+			}
+		}
+
+		//check if the player's move has created a winning row
+		//player wins if his mark covers an entire row, column, or diagonal
+		function checkWinners(i,j)
+		{
+			//check row and column of last placed mark
+			for(x=0;x<size && board[i][x]==player;x++){}
+			for(y=0;y<size && board[y][j]==player;y++){}
+
+			//if move is on top-left -> bottom-right diagonal
+			var d1,d2;
+			if(i==j)
+				for(d1=0;d1<size && board[d1][d1]==player;d1++){}
+			//if move is on top-right -> bottom-left diagonal
+			if(i==size-1-j);
+				for(d2=0;d2<size && board[d2][size-1-d2]==player;d2++){}
+
+			//if any of the rows are complete
+			if(x==size || y==size || d1==size || d2==size)
+			{
+				var winners = $('');
+
+				//select all the squares involved a winning row (there might be more than one winning row)
+				for(m=0;m<size;m++){ 
+					if(x==size)
+						winners=winners.add(elem.find('#board tr').eq(i).find('td').eq(m));
+					if(y==size)
+						winners=winners.add(elem.find('#board tr').eq(m).find('td').eq(j));
+					if(d1==size)
+						winners=winners.add(elem.find('#board tr').eq(m).find('td').eq(m));
+					if(d2==size)
+						winners=winners.add(elem.find('#board tr').eq(m).find('td').eq(size-1-m));
+				}
+
+				//add exclusive winner's styling to the winning squares
+				winners.addClass('win');
+
+				//update the status
+				elem.find('#status').append(' wins').find('#name').toggleClass('player0 player1');
+
+				//make the board inert, display the reset message
+				endGame();
+			}
+		}
+
+		function endGame()
+		{
+			//unhook the click events for making moves
+			elem.undelegate('#board td:not(.player0,.player1)','click');
+	
+			//create the reset message
+			elem.append('<input id="reset"/>');
+			var reset = elem.find('#reset');
+			reset
+				.val('Play Again')
+				.click(function() {			//when the reset message is clicked, start another game of the same size
+					elem.ttrGame(size);
+				})
+				.fadeIn(400)				//fade it in because THIS IS THE TWENTY-FIRST CENTURY AND EVERYTHING GOES SWOOSH NOW
+			;
+
+			elem.find('#board td').css('cursor','default');	//can't click on squares any more, so don't show the pointer
+
+			//game is done, so make a note not to do any more game logic
+			done = true;
+		}
+
+		//squares that haven't already been marked get marked when you click on them
+		elem.delegate('#board td:not(.player0,.player1)','click',function() {
+
+			//get position of clicked square
+			var j = size - $(this).nextAll().length - 1;
+			var i = size - $(this).parent().nextAll().length - 1;
+
+			//update board states
+			board[i][j] = player;
+			$(this).addClass(player ? 'player0' : 'player1');
+
+			checkReversi(i,j);
+
+			checkWinners(i,j);
+			
+			//if as many clicks have been done as there are squares, it's a draw
+			clicks++;
+			if(clicks==size*size && !done)
+			{
+				elem.find('#status').html("It's a draw!");
+				endGame();
+			}
+
+
+			//swap active player
+			player = !player;
+			elem.find('#name').toggleClass('player0 player1');
+		});
+
+	});
 };
 
-function win(player) {
 
-  var win = false;
-  var sum = 4*player;
+$('#game').ttrGame(3);
 
-  // rows
-  for (r=0;r<4;r++) {
-    if ($('#c'+r+'0').data('player') +
-        $('#c'+r+'1').data('player') +
-        $('#c'+r+'2').data('player') +
-        $('#c'+r+'3').data('player')==sum) {
-      win = true;
-      $('#c'+r+'0'+',#c'+r+'1'+',#c'+r+'2'+',#c'+r+'3').addClass('win');
-    }
-  }
-  // cols
-  for (c=0;c<4;c++) {
-    if ($('#c'+'0'+c).data('player') +
-        $('#c'+'1'+c).data('player') +
-        $('#c'+'2'+c).data('player') +
-        $('#c'+'3'+c).data('player')==sum) {
-      win = true;
-      $('#c0'+c+',#c1'+c+',#c2'+c+',#c3'+c).addClass('win');
-    }
-  }
-  // diag
-  if ($('#c00').data('player') +
-      $('#c11').data('player') +
-      $('#c22').data('player') +
-      $('#c33').data('player')==sum) {
-    win = true;
-    $('#c00,#c11,#c22,#c33').addClass('win');
-  }
-  if ($('#c30').data('player') +
-      $('#c21').data('player') +
-      $('#c12').data('player') +
-      $('#c03').data('player')==sum) {
-    win = true;
-    $('#c30,#c21,#c12,#c03').addClass('win');
-  }
+});
 
-  if (win) {
-    $('#tto-game').data('playing',false);
-    $('#reset').fadeIn();
-    $('.row').css({opacity: 0.7});
-  }
-}
-
-function nextp(player) {
-  
-  var next = (player+1) % 2;
-  $('#player').removeClass('player'+player).addClass('player'+next);
-}
-
-function reset() {
-
-  $('#tto-game').data('player',0).data('playing',true);
-
-  $('.cell').each(function(i) {
-    $(this).data('player',999).data('col',(i)%4).data('row',(i-((i)%4))/4).removeClass('player0').removeClass('player1').removeClass('win');
-  });
-
-  $('#player').removeClass('player1').addClass('player0');
-
-  $('#reset').hide();
-
-  $('.row').css({opacity: 1});
-}
